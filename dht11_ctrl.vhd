769daf1ab77ae22f514b21067dd421c9640e648a
coletta <marco.coletta@eurecom.fr>
2017-05-18 15:06:51 +0200

    Start dht11_ctrl

--- dht11_ctrl.vhd
+++ dht11_ctrl.vhd
@@ -1,5 +1,3 @@
--- DTH11 controller
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -13,7 +11,7 @@ entity dht11_ctrl is
 	);
 	port(
 		clk:      in  std_ulogic;
-		srstn:    in  std_ulogic; -- Active low synchronous reset
+		rst:      in  std_ulogic;
 		start:    in  std_ulogic;
 		data_in:  in  std_ulogic;
 		data_drv: out std_ulogic;
@@ -25,6 +23,53 @@ end entity dht11_ctrl;
 
 architecture rtl of dht11_ctrl is
 
-begin
+  signal pulse, en, init_enable, shift_enable, data, final_cnt, final_count, out_second_comparator : std_ulogic;
+  signal out_comparator : std_ulogic_vector(1 DOWNTO 0);
+  signal init_counter, margin, threshold_comp : integer;
+
+  begin
+
+    dp: entity work.datapath(beh)
+    port map(
+      data_in               => data_in,    
+      data_drv              => data_drv,               
+      rst                   => rst,  	               
+      clk	           	  => clk,
+      en                    => en,
+      init_enable           => init_enable,
+      shift_enable          => shift_enable,
+      init_counter          => init_counter,
+      margin                => margin,
+      threshold_comp        => threshold_comp,
+      data                  => data,
+      final_cnt	          => final_cnt,
+      final_count	          => final_count,
+      pulse		  => pulse,
+      out_comparator        => out_comparator,
+      out_second_comparator => out_second_comparator,
+      do                    => do
+    );
+
+    CU: entity work.CU(behav)
+    port map(
+      CLK	                   => clk,
+      RST                    => rst,
+      FINAL_COUNTER          => final_count,
+      FINAL_CNT              => final_cnt,
+      PULSE                  => pulse,
+      OUT_DEBOUNCER          => start,
+      OUT_COMPARATOR         => out_comparator,
+      OUT_SECOND_COMPARATOR  => out_second_comparator,
+      EN                     => en,
+      INITIAL_ENABLE         => init_enable,
+      SHIFT_ENABLE           => shift_enable,
+      BUSY_BIT               => b,
+      PROTOCOL_ERROR         => pe,
+      INIT_COUNTER           => init_counter,
+      MARGIN                 => margin,
+      THRESHOLD_COMP         => threshold_comp,
+      DATA	           => data,
+      DATA_DRV               => data_drv
+    );
 
 end architecture rtl;
